bb3e814a397ae3588e2c898181b1d75ceadc0b4a
Thomas Dissert <dissertthomas@gmail.com>
2022-10-19 22:35:30 +0200

    added environment variables REDIS_URL and BASE_URL

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
     NODE_ENV: process.env.NODE_ENV,
-    REDIS_URL: process.env.NODE_ENV === 'production' ? 'redis://lab.fritz.box:46379' : 'redis://localhost:6379',
-    BASE_URL: process.env.NODE_ENV === 'production' ? 'https://valorant.thomasdissert.com' : 'http://localhost:3000',
+    REDIS_URL: process.env.NODE_ENV === 'production' ? process.env.REDIS_URL : 'redis://localhost:6379',
+    BASE_URL: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL : 'http://localhost:3000',
   }
 
 }
